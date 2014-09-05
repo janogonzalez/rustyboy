c60cd7388bce947ec9c1d446c24d8b221f7c1f3f
Jano González <info@janogonzalez.com>
2014-09-06 01:23:11 +0200

    Add RAM to memory abstraction

--- src/memory.rs
+++ src/memory.rs
@@ -1,20 +1,41 @@
 use rom;
 
 pub struct Memory {
-    pub rom: rom::Rom
+    pub rom: rom::Rom,
+    ram: [u8, ..0x1000],
+    hram: [u8, ..0x80]
 }
 
 impl Memory {
     pub fn new(rom: rom::Rom) -> Memory {
         Memory {
-            rom: rom
+            rom: rom,
+            ram: [0x00, ..0x1000],
+            hram: [0x00, ..0x80]
         }
     }
 
     pub fn read_byte(&self, address: u16) -> u8 {
         match address {
             0x0000..0x3FFF => self.rom.bytes[address as uint],
+            0xC000..0xDFFF => self.ram[address as uint - 0xC000],
+            0xFF80..0xFFFF => self.hram[address as uint - 0xFF80],
             _ => 0x00
+        }
+    }
+
+    pub fn write_byte(&mut self, address: u16, value: u8) {
+        match address {
+            0xC000..0xDFFF => {
+                self.ram[address as uint - 0xC000] = value
+            },
+            0xFF00..0xFF7F => {
+                println!("  implement I/O ports stuff...")
+            },
+            0xFF80..0xFFFF => {
+                self.hram[address as uint - 0xFF80] = value
+            },
+            _ => println!("Can't write to memory address {:#06x}", address)
         }
     }
 
